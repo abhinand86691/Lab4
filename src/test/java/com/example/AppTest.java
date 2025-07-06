b2ce7477675b4730f10ee8feafd4ef83250fd863
abhinand86691 <2021wb86691@wilp.bits-pilani.ac.in>
2025-07-06 13:22:55 +0530

    Update AppTest.java

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -1,13 +1,10 @@
 package com.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class AppTest {
-    @Test
-    public void testApp() {
-        assertEquals("Hello", "Hello");
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Jenkins CI!");
     }
 }
+
 
 
